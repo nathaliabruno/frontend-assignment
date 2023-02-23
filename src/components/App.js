35a33a7bf963be5cc2c48fa11ef44b627f200cb0
Nathalia Bruno <nathi.bruno@gmail.com>
2023-02-23 20:26:50 -0300

    Add input UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import Button from "./Button"
+import Input from "./Input"
 
 const App = () => (
   <div>
     timescale
     <div>
       <Button label="Primary" primary />
-      <Button label="Secondary" />
-      <Button label="disabled" disabled />
+      <Input
+        type="text"
+        label="Email address"
+        placeholder="smith@smithandco.com"
+        name="email"
+        errorMessage="Invalid email"
+        error={false}
+      />
+      <Input
+        type="password"
+        label="Create password"
+        placeholder="6 caracteres or more"
+        name="password"
+        errorMessage="password too short"
+        error={false}
+      />
     </div>
   </div>
 )
