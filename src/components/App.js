61005947e3c08c3aa18d5079cd2e7d656837fb10
Nathalia Bruno <nathi.bruno@gmail.com>
2023-02-23 22:54:11 -0300

    Add topbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import Form from "./Form"
+import TopBar from "./TopBar"
 
 const App = () => (
   <div>
+    <TopBar />
     <Form />
   </div>
 )
--- src/custom-hook.js
+++ src/custom-hook.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import { useState } from "react"
 
 export const useForm = (options) => {
   const [data, setData] = useState(options?.initialValues || {})
