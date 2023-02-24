1a9ba9830e2db628bd5a3eeaf04ba1983937f56b
Nathalia Bruno <nathi.bruno@gmail.com>
2023-02-23 22:59:52 -0300

    update validation for each field

--- src/custom-hook.js
+++ src/custom-hook.js
@@ -4,8 +4,66 @@ export const useForm = (options) => {
   const [data, setData] = useState(options?.initialValues || {})
   const [errors, setErrors] = useState({})
 
+  const validate = (validations, checkKey) => {
+    if (validations) {
+      let valid = true
+      const newErrors = {}
+      if (checkKey) {
+        const value = data[checkKey]
+        const validation = validations[checkKey]
+        if (validation?.required?.value && !value) {
+          valid = false
+          newErrors[checkKey] = validation?.required?.message
+        }
+
+        const pattern = validation?.pattern
+        if (pattern?.value && !RegExp(pattern.value).test(value)) {
+          valid = false
+          newErrors[checkKey] = pattern.message
+        }
+
+        const custom = validation?.custom
+        if (custom?.isValid && !custom.isValid(value)) {
+          valid = false
+          newErrors[checkKey] = custom.message
+        }
+      } else {
+        for (const key in validations) {
+          const value = data[key]
+          const validation = validations[key]
+          if (validation?.required?.value && !value) {
+            valid = false
+            newErrors[key] = validation?.required?.message
+          }
+
+          const pattern = validation?.pattern
+          if (pattern?.value && !RegExp(pattern.value).test(value)) {
+            valid = false
+            newErrors[key] = pattern.message
+          }
+
+          const custom = validation?.custom
+          if (custom?.isValid && !custom.isValid(value)) {
+            valid = false
+            newErrors[key] = custom.message
+          }
+        }
+      }
+
+      if (!valid) {
+        setErrors(newErrors)
+        return
+      }
+      setErrors({})
+      if (options?.onSubmit) {
+        options.onSubmit()
+      }
+    }
+  }
+
   const handleChange = (key, sanitizeFn) => (e) => {
     const value = sanitizeFn ? sanitizeFn(e.target.value) : e.target.value
+    validate(options?.validations, key)
     setData({
       ...data,
       [key]: value,
@@ -15,42 +73,7 @@ export const useForm = (options) => {
   const handleSubmit = async (e) => {
     e.preventDefault()
 
-    const validations = options?.validations
-
-    if (validations) {
-      let valid = true
-      const newErrors = {}
-      for (const key in validations) {
-        const value = data[key]
-        const validation = validations[key]
-        if (validation?.required?.value && !value) {
-          valid = false
-          newErrors[key] = validation?.required?.message
-        }
-
-        const pattern = validation?.pattern
-        if (pattern?.value && !RegExp(pattern.value).test(value)) {
-          valid = false
-          newErrors[key] = pattern.message
-        }
-
-        const custom = validation?.custom
-        if (custom?.isValid && !custom.isValid(value)) {
-          valid = false
-          newErrors[key] = custom.message
-        }
-      }
-
-      if (!valid) {
-        setErrors(newErrors)
-        return
-      }
-      setErrors({})
-    }
-
-    if (options?.onSubmit) {
-      options.onSubmit()
-    }
+    validate(options?.validations)
   }
 
   return {
